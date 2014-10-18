6319c76bd64a202167d0e185c6f2e38670c4ca6d
Florie Coggins <florie.coggins@gmail.com>
2014-10-17 19:00:50 -0700

    Worked through exercises in part 2 of Hackbright App

--- hackbright_app.py
+++ hackbright_app.py
@@ -11,10 +11,68 @@ def get_student_by_github(github):
 Student: %s %s
 Github account: %s"""%(row[0], row[1], row[2])
 
+def get_project_by_title(project):
+    query = """SELECT title, description, max_grade FROM projects WHERE title = ?"""
+    DB.execute(query, (project,))
+    row = DB.fetchone()
+    print """\
+Project Title: %s
+Description: %s
+Maximum Grade: %s"""%(row[0], row[1], row[2])
+
+def get_students_grade_by_project(first_name, last_name, project):
+    query = """SELECT first_name, last_name, project_title, grade 
+    FROM students
+    JOIN grades
+    ON (github = student_github)
+    JOIN projects
+    ON (title = project_title)
+    WHERE first_name = ?
+    AND last_name = ?
+    AND project_title = ?"""
+    DB.execute(query, (first_name, last_name, project))
+    row = DB.fetchone()
+    print """\
+Student: %s %s
+Project: %s
+Grade: %s"""%(row[0], row[1], row[2], row[3])
+
 def connect_to_db():
     global DB, CONN
     CONN = sqlite3.connect("hackbright.db")
     DB = CONN.cursor()
+
+def make_new_student(first_name, last_name, github):
+    query = """INSERT INTO students VALUES (?, ?, ?)"""
+    DB.execute(query, (first_name, last_name, github))
+    CONN.commit()
+    print "Successfully added student: %s %s"%(first_name, last_name)
+
+def assign_grade(first_name, last_name, grade, title):
+    query1 = """SELECT github FROM students WHERE first_name = ? and 
+    last_name = ?"""
+    DB.execute(query1, (first_name, last_name))
+    row = DB.fetchone()
+    github = row[0]
+    query = """INSERT INTO  grades VALUES (?, ?, ?) """
+    DB.execute(query, (github, title, grade))
+    CONN.commit()
+    print "Successfully added %s %s grade for %s project"%(first_name, last_name, title)
+    print "The grade entered was %s"%(grade)
+
+def show_grades(first_name, last_name):
+    query = """SELECT first_name, last_name, project_title, grade FROM students
+    JOIN grades ON (github = student_github)
+    WHERE first_name = ?
+    AND last_name = ?"""
+    DB.execute(query, (first_name, last_name))
+    row = DB.fetchall()
+    print "Grades for student: %s %s"%(row[0][0],row[0][1])
+    i=0
+    while i in range(len(row)):
+        print row[i][2], row[i][3]
+        i += 1
+    
 
 def main():
     connect_to_db()
@@ -29,6 +87,14 @@ def main():
             get_student_by_github(*args) 
         elif command == "new_student":
             make_new_student(*args)
+        elif command == "projects":
+            get_project_by_title(*args)
+        elif command == "grade":
+            get_students_grade_by_project(*args)
+        elif command == "assign_grade":
+            assign_grade(*args)
+        elif command == "show_grades":
+            show_grades(*args)
 
     CONN.close()
 
